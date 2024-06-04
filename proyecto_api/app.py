4c2edfff0f999bc3ddf7d1ff8f13f6fde3932c19
bono-neer <167357273+bono-neer@users.noreply.github.com>
2024-06-04 15:22:19 -0300

    #18 API Mascotas Perdidas

--- proyecto_api/app.py
+++ proyecto_api/app.py
@@ -22,10 +22,41 @@ def obtener_tabla_caracteristicas():
                 if row.animal not in data:
                     data[row.animal] = {}
                 data[row.animal][row.caracteristica] = data[row.animal].get(row.caracteristica, []) + [row.valor]
-            return jsonify(data)
+            return jsonify(data), 200
     except SQLAlchemyError as err:
-        return jsonify(str(err.__cause__))
+        return jsonify(str(err.__cause__)), 500
+
+@app.route('/mascotas_perdidas', methods=["POST"])
+def mascotas_perdidas():
     
+    if request.method == "POST":
+        
+        datos_mascota = request.get_json()
+
+        columnas = []
+        valores = []
+
+        # Se agregan solo columnas y datos los cuales no tienen un valor nulo
+        for columna, dato in datos_mascota.items():
+            if dato:
+                columnas.append(columna)
+                valores.append(f"'{dato}'")
+
+        # Gracias a los bucles, me aseguro que se van a añadir los datos a las columnas correspondientes
+        clausula_insert = "INSERT INTO animales_perdidos (" + ", ".join(columnas) + ") "
+        clausula_values = "VALUES (" + ", ".join(valores) + ");"
+
+        # Junto ambas partes para formar la query
+        query = clausula_insert + clausula_values
+
+        try:
+            with engine.begin() as conexion:
+                conexion.execute(text(query))
+        except SQLAlchemyError as err:
+            return jsonify(str(err.__cause__)), 500
+        
+        return jsonify({'mensaje': "Los datos se agregaron correctamente"}), 201
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=PUERTO_API)
 
