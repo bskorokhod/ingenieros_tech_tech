56e30df88dfdee3d93f33e8b0a8e4d7ac3d29d26
Pilar <pmontilla@fi.uba.ar>
2024-06-04 16:23:45 -0300

    #38 Errorhandler 404 & 500

--- proyecto_vistas/app.py
+++ proyecto_vistas/app.py
@@ -8,5 +8,13 @@ PUERTO_APP = 5000
 def home():
     return render_template('home.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(port=PUERTO_APP)
