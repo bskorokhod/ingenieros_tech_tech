f44ecd2044b459a717df9963c25ff0a99785e6f6
Pilar <pmontilla@fi.uba.ar>
2024-06-06 16:13:55 -0300

    #38 Errorhandler 404 & 500 Correcciones
    Se cambió el nombre de los archivos html de 404.html y 500.html a error_404.html y error_500.html respectivamente.

--- proyecto_vistas/app.py
+++ proyecto_vistas/app.py
@@ -10,11 +10,11 @@ def home():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('error_404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('error_500.html'), 500
 
 if __name__ == '__main__':
     app.run(port=PUERTO_APP)
