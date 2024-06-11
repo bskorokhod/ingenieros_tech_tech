e083ec6470732e8038744e00d43baf7c76770467
Nicolas-Olivieri <olivierinik22@gmail.com>
2024-06-11 15:31:35 -0300

    #19 API refugios
    Añado el servicio '/refugios' de la API. Este recibe métodos GET, POST, PATCH y DELETE.
    - Con GET devuelve los refugios presentes en la TABLA_REFUGIOS, filtrándolos dependiendo del body de la request.
    - Con POST permite agregar un refugio a la tabla con la información recibida en el body de la request.
    - Con PATCH se permite modificar el valor de la columna 'aceptado' para un refugio cuya 'id' fue recibida en el body de la request.
    - Con DELETE se elimina de la tabla un refugio cuya 'id' fue recibida en el body de la request.

--- proyecto_api/app.py
+++ proyecto_api/app.py
@@ -7,6 +7,8 @@ from sqlalchemy.exc import SQLAlchemyError
 CONECTOR_SQL = "mysql+mysqlconnector://root@localhost/mascotas"
 QUERY_OBTENER_CARACTERISTICAS = "SELECT * FROM caracteristicas_mascotas" # Probar seleccionando sólo las columnas que no tienen id 
 
+# TABLA_REFUGIOS = "refugios"
+
 engine = create_engine(CONECTOR_SQL)
 
 app = Flask(__name__)
@@ -26,6 +28,78 @@ def obtener_tabla_caracteristicas():
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__))
     
+@app.route('/refugios', methods = ['GET','POST','PATCH', 'DELETE'])
+def refugios():
+    if request.method == "POST": # se busca añadir un refugio y se recibe un body json con la data del formulario
+        informacion = request.get_json()
+        # Queda pendiente validar si las claves se encuentran en el json
+        nombre_refugio = informacion.get("nombre")
+        coord_x = informacion.get("coord_x")
+        coord_y = informacion.get("coord_y")
+        telefono = informacion.get("telefono")
+        direccion = informacion.get("direccion")
+        query = f"INSERT INTO {TABLA_REFUGIOS} (nombre, coord_x, coord_y, telefono, direccion) VALUES '{nombre_refugio}', '{coord_x}', '{coord_y}', '{telefono}', '{direccion}';" 
+        try:
+            with engine.begin() as conexion:
+                resultado = conexion.execute(text(query))
+                # al realizar engine.begin() al finalizar el bloque se agrega automáticamente el engine.commit() tras realizar los cambios guardándolos
+                return jsonify({"mensaje":"exito"}), 201 
+        except SQLAlchemyError as err:
+            return jsonify(str(err.__cause__)), 500
+
+    if request.method == "GET":
+        informacion = request.get_json()
+        if informacion == {}: # o  'if not informacion'
+            filtro="TRUE"
+        elif informacion.get("aceptado") == "false":
+            filtro="FALSE"
+        else: # Body inválido
+            return jsonify({"mensaje":"Body inválido"}), 400 # Bad request
+        query = f"SELECT * FROM {TABLA_REFUGIOS} WHERE aceptado = {filtro};"
+        try:
+            with engine.connect() as conexion:
+                resultado = conexion.execute(text(query))
+        except SQLAlchemyError as err:
+            return jsonify(str(err.__cause__)), 500
+        datos = []
+        for fila in resultado:
+            dato = {}
+            dato["nombre"] = fila.nombre
+            dato["telefono"] = fila.telefono
+            dato["direccion"] = fila.direccion
+            dato["coordx"] = fila.coordx
+            dato["coordy"] = fila.coordy
+            datos.append(dato)
+        return jsonify(datos), 200
+
+    # si llegó a este punto, el metodo o es PATCH, o es DELETE
+    informacion = request.get_json()
+    id_del_refugio = informacion.get("id", -1)
+    if id_del_refugio == -1: # nunca va a existir un id '-1'
+        return jsonify({"mensaje":"Body inválido"}), 400 # Bad request
+
+    query_validacion = f"SELECT * FROM {TABLA_REFUGIOS} WHERE 'id' = {id_del_refugio};"
+    try:
+        with engine.connect() as conexion:
+            resultado = conexion.execute(text(query_validacion))
+    except SQLAlchemyError as err:
+        return jsonify(str(err.__cause__)), 500
+
+    if resultado.rowcount == 0: # No hay ningún refugio con ese id
+        return jsonify({"mensaje":"Body inválido"}), 400 
+    
+    if request.method == "PATCH":
+        query = f"UPDATE {TABLA_REFUGIOS} SET aceptado = TRUE WHERE id = {id_del_refugio};"
+    else: # metodo == "DELETE"
+        query = f"DELETE FROM {TABLA_REFUGIOS} WHERE id = {id_del_refugio};"
+    try:
+        with engine.begin() as conexion:
+            resultado = conexion.execute(text(query))
+            # al realizar engine.begin() al finalizar el bloque se agrega automáticamente el engine.commit() tras realizar los cambios guardándolos
+            return jsonify({"mensaje":"exito"}), 201 
+    except SQLAlchemyError as err:
+        return jsonify(str(err.__cause__)), 500
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=PUERTO_API)
 
