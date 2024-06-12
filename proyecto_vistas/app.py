b9222bfaa4034f3075ed5378863fc0507b3ebed9
Bogdan <bskorokhod@fi.uba.ar>
2024-06-12 14:26:54 -0300

    #10 Base formulario aceptado/denegado / #41 Endpoint "/aceptado"
    Agrego el template de formulario aceptado/denegado que se adapta según el formulario completado (especificado por parámetro en app.py).
    Agrego el endpoint /aceptado que recibe el parámetro de formulario completado y renderiza el template de formulario aceptado/denegado.
    - Admite los métodos GET y POST.
    - Si el método es POST:
        1. Se envía un POST a la API con el id de la mascota.
            - En caso de haber errores se redirecciona al error handler 500.
        2. Se renderiza el template de formulario aceptado/denegado.
    - Si el método es GET:
        1. Se renderiza el template de formulario aceptado/denegado.

--- proyecto_vistas/app.py
+++ proyecto_vistas/app.py
@@ -8,6 +8,22 @@ PUERTO_APP = 5000
 def home():
     return render_template('home.html')
 
+@app.route("/aceptado/", defaults={"formulario": None})
+@app.route("/aceptado/<formulario>", methods=["GET", "POST"])
+def aceptado(formulario):
+    # si el formulario es de reencuentro, se envía un POST a la API con el id de la mascota
+    if request.method == "POST":
+        id_mascota = request.form.get("id_mascota")
+        data = {"id": id_mascota}
+
+        response = requests.post(HOST_API + "/reportes_reencuentro", json=data)
+
+        # si falla la petición, se redirige a la página de error 500
+        if response.status_code != 201:
+            return redirect("internal_server_error", e=response.status_code)
+
+    return render_template("aceptado.html", form=formulario)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('base_errores.html', numero_error=404), 404
