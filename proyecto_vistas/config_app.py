7871964b5c6823965fad99a5fa5fa2b7bfd61483
NachoJSD <holtmannacho@gmail.com>
2024-06-15 23:06:30 -0300

    #45 - Añadidas constantes de límites
    Se añadieron los límites máximos de elementos que se muestran a la vez en vistas determinadas.

--- proyecto_vistas/config_app.py
+++ proyecto_vistas/config_app.py
@@ -8,3 +8,9 @@ ENDPOINT_API_REFUGIO = '/refugios'
 ENDPOINT_API_PERDIDAS = '/mascotas_perdidas'
 ENDPOINT_API_REPORTES = '/reportes_reencuentro'
 ENDPOINT_API_CARACTERISTICAS = '/caracteristicas_mascotas'
+
+# Límites máximos de elementos que se muestran por página
+LIMITE_MASCOTAS = 9             #  /encontrado
+LIMITE_REPORTES_REENCUENTRO = 5 #  /admin
+LIMITE_REPORTES_REFUGIOS = 5    #  /admin
+LIMITE_REFUGIOS = 12            #  /refugios
