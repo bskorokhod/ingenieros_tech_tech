8dc80e3eb424ce93d64c8ef4e22417c9fff31288
NachoJSD <holtmannacho@gmail.com>
2024-06-15 21:52:19 -0300

    #33 - Refactorización de código y correcciones significativas
    1. Se añadió una descripción a la función del endpoint /admin.
    2. Se corrigieron las requests de método GET: ahora envían datos por parámetros en vez de JSON.
    3. Se emprolijó el código.
    4. Se adaptó el endpoint para añadir funcionalidad a la vista /admin de sólo mostrar un número limitado de elementos (SQL Pagination):
        a. Se solicitan datos adicionales dentro del forms que se recibe independientemente del método. Se tiene en cuenta que, en caso de no recibirse estos datos adicionales, se muestra siempre la primera página de resultados.
        b. Se envian datos adicionales por medio de las requests de método GET.
        c. Se envían variables adicionales al renderizar la vista /admin.
    5. Se agregaron algunos comentarios.

--- proyecto_vistas/app.py
+++ proyecto_vistas/app.py
@@ -9,73 +9,153 @@ def login():
 
 @app.route('/admin', methods=['POST', 'PATCH', 'PUT', 'DELETE'])
 def admin_config():
-    if request.method in ('POST', 'PATCH', 'PUT', 'DELETE'):
+    """
+    FLUJO GENERAL DEL ENDPOINT `/admin`
 
-        usuario = request.form.get("usuario")
-        contraseña = request.form.get("contraseña")
+    Si la solicitud viene desde `/login_admin`...
+    Se espera recibir un formulario que tenga datos dentro de las claves:
+        1. `usuario`
+        2. `contraseña`
 
-        response_sesion = requests.get(HOST_API + ENDPOINT_API_LOGIN, json={"user": usuario, "password": contraseña})
+    Si la solicitud viene desde esta misma página (`/admin`), es decir, se recarga la página...
+    Se espera recibir un formulario que tenga datos dentro de las claves:
+        1. `usuario`
+        2. `contraseña`
+        3. `tipo`
+        4. `accion`
+        5. `id` / `id_mascota` / `id_reporte` (basado en los valores de las anteriores 2 claves)
+        6. `pag_actual_refugios`
+        7. `pag_actual_reportes`
 
-        if response_sesion.status_code >= 500:
-            return internal_server_error(e=response_sesion.status_code)
+        Dependiendo de los valores de las claves 3 y 4 se envían requests:
+            a. 'refugio' + 'aceptar'  ==> PATCH a `ENDPOINT_API_REFUGIO`
+            b. 'refugio' + 'rechazar' ==> DELETE a `ENDPOINT_API_REFUGIO`
+            c. 'reencuentro' + 'aceptar'  ==> PUT a `ENDPOINT_API_REPORTES`
+            d. 'reencuentro' + 'rechazar' ==> PATCH a `ENDPOINT_API_REPORTES`
 
-        sesion = response_sesion.json()
+    En todos los casos se envían requests de método GET a:
+        I.   `ENDPOINT_API_LOGIN` valida claves 1 y 2
+        II.  `ENDPOINT_API_REFUGIO` obtiene solicitudes de refugios 
+        III. `ENDPOINT_API_REPORTES` obtiene reportes de reencuentros
 
-        if not isinstance(sesion, dict) or "code" not in sesion or not isinstance(sesion.get("code"), int) or sesion.get("code", -1) < 0 or sesion.get("code", 2) > 1:
-            return internal_server_error(e=500)
+    Si la sesión es válida se renderiza la vista `/admin`, sino se redirige a `/login_admin`.
+    """
 
-        if sesion == 1:
+    # Antes de cargar la página se verifica la sesión
+    usuario = request.form.get("usuario")
+    contraseña = request.form.get("contraseña")
 
-            if request.method in ('PATCH', 'PUT', 'DELETE'):
+    if usuario is None or contraseña is None:
+        return redirect(url_for('login'))
 
-                tipo = request.form.get("tipo").lower() # "refugio" / "reencuentro"
-                accion = request.form.get("accion").lower() # "aceptar" / "rechazar"
+    # Se envía el usuario y contraseña ingresados a la API y se recibe la validación
+    response_sesion = requests.get(HOST_API + ENDPOINT_API_LOGIN,
+                                    params={"user": usuario,
+                                            "password": contraseña})
 
-                if tipo == "refugio":
+    if response_sesion.status_code >= 500:
+        return internal_server_error(e=response_sesion.status_code)
 
-                    id = request.form.get("id").lower()
+    sesion = response_sesion.json()
 
-                    if accion == "aceptar":
-                        requests.patch(HOST_API + ENDPOINT_API_REFUGIO, json={"id": id})
+    if (not isinstance(sesion, dict) or
+        "code" not in sesion or
+        not isinstance(sesion.get("code"), int) or
+        sesion.get("code", -1) < 0 or
+        sesion.get("code", 2) > 1):
+        return internal_server_error(e=500)
 
-                    elif accion == "rechazar":
-                        requests.delete(HOST_API + ENDPOINT_API_REFUGIO, json={"id": id})
-                    
-                    else:
-                        return internal_server_error(e=500)
+    if sesion.get("code") == 1:
 
-                elif tipo == "reencuentro":
-                    
-                    if accion == "aceptar":
-                        id_mascota = request.form.get("id_mascota").lower()
-                        requests.put(HOST_API + ENDPOINT_API_REPORTES, json={"id mascota": id_mascota})
+        if request.method in ('PATCH', 'PUT', 'DELETE'):
 
-                    elif accion == "rechazar":
-                        id_reporte = request.form.get("id_reporte").lower()
-                        requests.patch(HOST_API + ENDPOINT_API_REPORTES, json={"id reporte": id_reporte})
-                    
-                    else:
-                        return internal_server_error(e=500)
+            tipo = request.form.get("tipo", "").lower() # "refugio" / "reencuentro"
+            accion = request.form.get("accion", "").lower() # "aceptar" / "rechazar"
+
+            if tipo == "refugio":
+
+                id = request.form.get("id", "").lower()
+
+                if id == "": return internal_server_error(e=500)
+
+                if accion == "aceptar":
+                    requests.patch(HOST_API + ENDPOINT_API_REFUGIO,
+                                    json={"id": id})
+
+                elif accion == "rechazar":
+                    requests.delete(HOST_API + ENDPOINT_API_REFUGIO,
+                                    json={"id": id})
                 
                 else:
                     return internal_server_error(e=500)
 
-            response_refugios = requests.get(HOST_API + ENDPOINT_API_REFUGIO, json={"aceptado": "false"})
+            elif tipo == "reencuentro":
+                
+                if accion == "aceptar":
+                    id_mascota = request.form.get("id_mascota", "").lower()
+
+                    if id_mascota == "": return internal_server_error(e=500)
+
+                    requests.put(HOST_API + ENDPOINT_API_REPORTES,
+                                    json={"id mascota": id_mascota})
+
+                elif accion == "rechazar":
+                    id_reporte = request.form.get("id_reporte", "").lower()
+
+                    if id_reporte == "": return internal_server_error(e=500)
+
+                    requests.patch(HOST_API + ENDPOINT_API_REPORTES,
+                                    json={"id reporte": id_reporte})
+                
+                else:
+                    return internal_server_error(e=500)
             
-            if response_refugios.status_code >= 500:
-                return internal_server_error(e=response_refugios.status_code)
+            else:
+                return internal_server_error(e=500)
 
-            response_reportes = requests.get(HOST_API + ENDPOINT_API_REPORTES, json={"fue_procesado": False})
+        # Esto se ejecuta siempre que la sesión sea válida, independientemente del método
 
-            if response_reportes.status_code >= 500:
-                return internal_server_error(e=response_reportes.status_code)
+        # En caso de enviarse un form desde login se carga la primera página
+        num_pagina_refugios = request.form.get("pag_actual_refugios", 1)
+        num_pagina_reportes = request.form.get("pag_actual_reportes", 1)
+        
+        if not num_pagina_refugios.isdigit() or not num_pagina_reportes.isdigit():
+            return internal_server_error(e=500)
 
-            refugios = response_refugios.json()
-            reportes = response_reportes.json()
 
-            return render_template('admin.html', usuario=usuario, contraseña=contraseña, refugios=refugios, reportes=reportes)
+        response_refugios = requests.get(HOST_API + ENDPOINT_API_REFUGIO,
+                                            params={"pag_refugios": int(num_pagina_refugios),
+                                                    "limite_refugios": LIMITE_REPORTES_REFUGIOS,
+                                                    "aceptado": "false"})
+        
+        if response_refugios.status_code >= 500:
+            return internal_server_error(e=response_refugios.status_code)
 
-        return redirect(url_for("login"))
+
+        response_reportes = requests.get(HOST_API + ENDPOINT_API_REPORTES,
+                                            params={"pag_reportes": int(num_pagina_reportes),
+                                                    "limite_reportes": LIMITE_REPORTES_REENCUENTRO,
+                                                    "fue_procesado": False})
+
+        if response_reportes.status_code >= 500:
+            return internal_server_error(e=response_reportes.status_code)
+
+        # PENDIENTE: CONSENSUAR CÓMO SE RECIBE SI HAY PÁGINA SIGUIENTE
+        #            VERIFICAR QUE LOS JSON CUMPLAN CON EL FORMATO ESPERADO
+        refugios = response_refugios.json()
+        reportes = response_reportes.json()
+
+        return render_template('admin.html',
+                                usuario = usuario,
+                                contraseña = contraseña,
+                                refugios = refugios,
+                                reportes = reportes,
+                                limite_refugios = LIMITE_REPORTES_REFUGIOS, # Sujeto a cambios
+                                limite_reportes = LIMITE_REPORTES_REENCUENTRO, # Sujeto a cambios
+                                pag_actual_refugios = num_pagina_refugios,
+                                pag_actual_reportes = num_pagina_reportes)
+
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(port=PUERTO_APP)
